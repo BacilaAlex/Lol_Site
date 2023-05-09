23c9fc5e270f2aa9dc52762d7f280922afebdaab
L3ntul <bacila.a2002@yahoo.com>
2023-05-09 13:39:33 +0300

    Am facut ceva, nu stiu ce

--- src/app/first-thing/first-thing.component.ts
+++ src/app/first-thing/first-thing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-first-thing',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-thing.component.css']
 })
 export class FirstThingComponent {
+  @ViewChild(".content") bg: any;
+
+  ChangeBackgroungImg(): void {
+  }
 
 }
